236b97b8296b43c0c9da1a463fcd8579a805daf1
James Pierson <jameswpbusiness@gmail.com>
2020-05-31 20:09:57 -0400

    Added background city img

--- src/Components/Advice.js
+++ src/Components/Advice.js
@@ -3,38 +3,43 @@ import axios from "axios";
 
 export default class Advice extends Component {
     // Sets advice to blank so it can be declared
-        state = { advice: ''};
+    state = { advice: '' };
 
-        // Signals if the axios calls sub-components properly
-        componentDidMount() {
-            this.fetchAdvice();
-        }
+    // Signals if the axios calls sub-components properly
+    componentDidMount() {
+        this.fetchAdvice();
+    }
 
-        // Function that calls API
-        fetchAdvice = () => {
-            axios.get("https://api.adviceslip.com/advice")
+    // Function that calls API
+    fetchAdvice = () => {
+        axios.get("https://api.adviceslip.com/advice")
             .then((response) => {
                 // Simplifies the response call
                 const { advice } = response.data.slip
 
                 // Sets state to the constant on line 18
-                this.setState({advice})
+                this.setState({ advice })
             })
             .catch((error) => {
                 // Calls error if API fails
                 console.error(error)
             });
-        }
- 
+    }
+
     render() {
         // Sets advice to the current state set by this.setState on line 20
         const { advice } = this.state;
-        return(
-        <div className="App">
-                    <h1>{advice}</h1>
-                    <button className="button" onClick={this.fetchAdvice}>New Quote</button>    
-        </div>
-            
+        return (
+            <div className="App">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>{advice}</h1>
+                    </div>
+                    <div className="card-body">
+                        <button className="button" onClick={this.fetchAdvice}>New Quote</button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
