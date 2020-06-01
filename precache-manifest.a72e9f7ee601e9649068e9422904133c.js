257aff1018a82c4b1ba148e96f0d625896bcfe36
James Pierson <jameswpbusiness@gmail.com>
2020-06-01 14:44:17 -0400

    Updates

---
+++ precache-manifest.a72e9f7ee601e9649068e9422904133c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b860bec75ab33502b8c8cf01eff5cc64",
+    "url": "/React-advice-slip/index.html"
+  },
+  {
+    "revision": "a2ee82ea82b0c5989a08",
+    "url": "/React-advice-slip/static/css/main.ed43e060.chunk.css"
+  },
+  {
+    "revision": "01c52af1fa95e917f50d",
+    "url": "/React-advice-slip/static/js/2.c50e8c1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-advice-slip/static/js/2.c50e8c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ee82ea82b0c5989a08",
+    "url": "/React-advice-slip/static/js/main.cccc300f.chunk.js"
+  },
+  {
+    "revision": "b51ba5958271b7ff484a",
+    "url": "/React-advice-slip/static/js/runtime-main.f33626c0.js"
+  },
+  {
+    "revision": "3419c3ee181fac9e2d806e77dde49d8c",
+    "url": "/React-advice-slip/static/media/cityimg.3419c3ee.jpeg"
+  }
+]);
